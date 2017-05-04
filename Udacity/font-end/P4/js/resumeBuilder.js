f728cabfe3870410967481c3f416d9af3513906e
syuu ki sei <syuukisei@users.noreply.github.com>
2017-05-04 23:10:53 +0800

    Update resumeBuilder.js
    对象的display方法包装着对应的展示代码。

--- Udacity/font-end/P4/js/resumeBuilder.js
+++ Udacity/font-end/P4/js/resumeBuilder.js
@@ -16,21 +16,38 @@ var bio = {
 	biopic: 'images/pic.jpg',
 	live:'深圳市宝安区固戍',
 	display: function() {
-		$("#topContacts,#footerContacts").append(HTMLmobile,HTMLemail,HTMLtwitter,HTMLgithub,HTMLlocation);
+		$("#header").prepend(HTMLheaderRole.replace("%data%",bio.role));
+		$("#header").prepend(HTMLheaderName.replace("%data%",bio.name));
+		$("#topContacts,#footerContacts").append(HTMLmobile.replace("%data%",bio.contacts.mobile),
+		HTMLemail.replace("%data%",bio.contacts.email),
+		HTMLgithub.replace("%data%",bio.contacts.github),
+		HTMLtwitter.replace("%data%",bio.contacts.twitter),
+		HTMLlocation.replace("%data%",bio.contacts.location));
    		// $("#header").append(HTMLskills);
-    		$("#header").append(HTMLbioPic,HTMLwelcomeMsg,HTMLskillsStart,HTMLskills);
+    		$("#header").append(HTMLbioPic,HTMLwelcomeMsg,HTMLskillsStart);
+    		bio.skills.forEach(function(e){$("#skills").append(`<li>${e}`)})
     		return console.log("bio is display.")
 		// body...
 	}
 }
+// var tnum = 0;
 var education = {
 	schools:[{
 		name: '深圳技师学院',
 		location: '深圳',
 		degree: '中专',
-		majors: '计算机动画',
+		majors:['计算机动画'],
+		// education.schools[0].majors
 		dates: '2009-2015',
 		url: 'http://www.ssti.net.cn/main/'
+	},{
+		name: '木棉湾实验学校',
+		location: '深圳',
+		degree: '初中',
+		majors:["-"],
+		// education.schools[0].majors
+		dates: '2007-2009',
+		url: ''
 	}],
 	onlineCourses:[{
 		title:'前端工程师纳米学位',
@@ -40,10 +57,27 @@ var education = {
 		url: 'https://cn.udacity.com/'
 	}],
 	display: function() {
-		$("#education").append(HTMLschoolStart,HTMLschoolName,HTMLschoolDegree,HTMLschoolDates,HTMLschoolLocation,HTMLschoolMajor);
-    		$("#education").append(HTMLonlineTitle,HTMLonlineSchool,HTMLonlineDates,HTMLonlineURL);
-    		return console.log("education is display.")
-		// body...
+		for(var i = 0;i<education.schools.length; i++){
+		$("#education").append(HTMLschoolName.replace("%data%",education.schools[i].name),
+		HTMLschoolDegree.replace("%data%",education.schools[i].degree),
+		HTMLschoolDates.replace("%data%",education.schools[i].dates),
+		HTMLschoolLocation.replace("%data%",education.schools[i].location),
+		HTMLschoolMajor.replace("%data%",education.schools[i].majors));
+		// console.log(education.schools[i].name);
+		// console.log(i);
+		}
+		for(var j = 0;j<education.onlineCourses.length; j++){
+		$("#education").append(HTMLonlineClasses.replace("%data%",education.onlineCourses[j].name),
+		HTMLonlineTitle.replace("%data%",education.onlineCourses[j].title),
+		// HTMLonlineTitle.replace("%url%",education.onlineCourses[j].classUrl),
+		HTMLonlineSchool.replace("%data%",education.onlineCourses[j].dates),
+		HTMLonlineDates.replace("%data%",education.onlineCourses[j].dates),
+		HTMLonlineURL.replace("%data%",education.onlineCourses[j].url)
+		// HTMLonlineURL.replace("%url%",education.onlineCourses[j].url)
+		);
+		// console.log(education.schools[i].name);
+		// console.log(i);
+		}
 	}
 }
 var work  = {
@@ -56,22 +90,50 @@ var work  = {
 		url: 'http://www.iwinad.com/'
 	}],
 	display: function() {
-		$("#workExperience").append(HTMLworkStart,HTMLworkEmployer,HTMLworkTitle,HTMLworkDates,HTMLworkDescription);
+		for(var i = 0;i<work.jobs.length; i++){
+		$("#workExperience").append(HTMLworkEmployer.replace("%data%",work.jobs[0].employer),
+		HTMLworkEmployer.replace("%url%",work.jobs[0].url),
+		HTMLworkTitle.replace("%data%",work.jobs[0].title),
+		HTMLworkDates.replace("%data%",work.jobs[0].dates),
+		HTMLworkLocation.replace("%data%",work.jobs[0].location),
+		HTMLworkDescription.replace("%data%",work.jobs[0].description)
+		);
     		return console.log("work is display.")
 		// body...
-	}
+	}}
 }
 var projects = {
-	projects:{
+	projects:[{
 		title: '华为快速部署家庭宽带项目',
 		dates: '201701-201702',
 		description: '因盈天下广告有限公司公司（设计外包公司）华为快速部署家庭宽带项目无法达到客户华为公司的需求，被返聘为短期合同工，负责展墙六大块屏幕的动画设计。完好解决华为方对该项目动画效果精致细腻的需求，得到华为领导客户的肯定评价。',
-		images: null,
+		// images: "null",
 		url:'http://www.huawei.com/cn/events/mwc/2017/maxmizing-network-value/home-broadband'
-	},
+	}],
 	display: function() {
-		$("#projects").append(HTMLprojectStart,HTMLprojectTitle,HTMLprojectDates,HTMLprojectDescription);
+		for(var i = 0;i<projects.projects.length; i++){
+		$("#projects").append(HTMLprojectTitle.replace("%data%",projects.projects[0].title),
+		// HTMLprojectTitle[0].replace("%url%",projects.projects.url),
+		HTMLprojectDates.replace("%data%",projects.projects[0].dates),
+		HTMLprojectDescription.replace("%data%",projects.projects[0].description)
+		// HTMLprojectImage.replace("%data%",projects.projects.images)
+		);
     		return console.log("projects is display.")
 		// body...
 	}
+	}
 }
+
+      console.log(`The jQuery version is ${$.fn.jquery}`);
+      var br = '<hr/>';
+      HTMLwelcomeMsg = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
+      HTMLbioPic = HTMLbioPic.replace("%data%",bio.biopic);
+      var skillsList ='<li>%data%</li>';
+      // googleMap = googleMap.replace("%data%",bio.live);
+      // var HTMLmap = '<div id="map"></div>';
+
+      $("#mapDiv").append(googleMap);
+      $('#workExperience').addClass('center-content')
+      $('#projects').addClass('center-content')
+      $('#education').addClass('center-content')
+      $('#workExperience').addClass('center-content')
