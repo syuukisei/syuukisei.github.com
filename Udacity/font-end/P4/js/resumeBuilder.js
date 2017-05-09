cce58cb8dc0c0da8d5eefcd6dfcd2ca2a4f903e6
syuu ki sei <syuukisei@users.noreply.github.com>
2017-05-09 21:19:51 +0800

    Update resumeBuilder.js

--- Udacity/font-end/P4/js/resumeBuilder.js
+++ Udacity/font-end/P4/js/resumeBuilder.js
@@ -61,19 +61,19 @@ var education = {
 		$("#education").append(HTMLschoolName.replace("%data%",education.schools[i].name),
 		HTMLschoolDegree.replace("%data%",education.schools[i].degree),
 		HTMLschoolDates.replace("%data%",education.schools[i].dates),
-		HTMLschoolLocation.replace("%data%",education.schools[i].location),
-		HTMLschoolMajor.replace("%data%",education.schools[i].majors));
-		// console.log(education.schools[i].name);
-		// console.log(i);
+		HTMLschoolLocation.replace("%data%",education.schools[i].location));
+			for(var j = 0;j<education.schools[i].majors.length; j++){
+			$("#education").append(HTMLschoolMajor.replace("%data%",education.schools[i].majors[j]))
+			}
 		}
-		for(var j = 0;j<education.onlineCourses.length; j++){
-		$("#education").append(HTMLonlineClasses.replace("%data%",education.onlineCourses[j].name),
-		HTMLonlineTitle.replace("%data%",education.onlineCourses[j].title),
-		// HTMLonlineTitle.replace("%url%",education.onlineCourses[j].classUrl),
-		HTMLonlineSchool.replace("%data%",education.onlineCourses[j].dates),
-		HTMLonlineDates.replace("%data%",education.onlineCourses[j].dates),
-		HTMLonlineURL.replace("%data%",education.onlineCourses[j].url)
-		// HTMLonlineURL.replace("%url%",education.onlineCourses[j].url)
+		for(var k = 0;k<education.onlineCourses.length; k++){
+		$("#education").append(HTMLonlineClasses.replace("%data%",education.onlineCourses[k].name),
+		HTMLonlineTitle.replace("%data%",education.onlineCourses[k].title),
+		// HTMLonlineTitle.replace("%url%",education.onlineCourses[k].classUrl),
+		HTMLonlineSchool.replace("%data%",education.onlineCourses[k].dates),
+		HTMLonlineDates.replace("%data%",education.onlineCourses[k].dates),
+		HTMLonlineURL.replace("%data%",education.onlineCourses[k].url)
+		// HTMLonlineURL.replace("%url%",education.onlineCourses[k].url)
 		);
 		// console.log(education.schools[i].name);
 		// console.log(i);
@@ -92,7 +92,7 @@ var work  = {
 	display: function() {
 		for(var i = 0;i<work.jobs.length; i++){
 		$("#workExperience").append(HTMLworkEmployer.replace("%data%",work.jobs[0].employer),
-		//HTMLworkEmployer.replace("%url%",work.jobs[0].url),
+		// HTMLworkEmployer.replace("%url%",work.jobs[0].url),
 		HTMLworkTitle.replace("%data%",work.jobs[0].title),
 		HTMLworkDates.replace("%data%",work.jobs[0].dates),
 		HTMLworkLocation.replace("%data%",work.jobs[0].location),
@@ -107,7 +107,7 @@ var projects = {
 		title: '华为快速部署家庭宽带项目',
 		dates: '201701-201702',
 		description: '因盈天下广告有限公司公司（设计外包公司）华为快速部署家庭宽带项目无法达到客户华为公司的需求，被返聘为短期合同工，负责展墙六大块屏幕的动画设计。完好解决华为方对该项目动画效果精致细腻的需求，得到华为领导客户的肯定评价。',
-		// images: "null",
+		images: ["images/route.jpg"],
 		url:'http://www.huawei.com/cn/events/mwc/2017/maxmizing-network-value/home-broadband'
 	}],
 	display: function() {
@@ -118,6 +118,9 @@ var projects = {
 		HTMLprojectDescription.replace("%data%",projects.projects[0].description)
 		// HTMLprojectImage.replace("%data%",projects.projects.images)
 		);
+		for(var j = 0;j<projects.projects[i].images.length; j++){
+		$("#projects").append(HTMLprojectImage.replace("%data%",projects.projects[i].images[j]))
+		}
     		return console.log("projects is display.")
 		// body...
 	}
@@ -137,3 +140,13 @@ var projects = {
       $('#projects').addClass('center-content')
       $('#education').addClass('center-content')
       $('#workExperience').addClass('center-content')
+
+      // $('#workExperience').addClass('center-content')
+      // $('#projects').addClass('center-content')
+      // $('#education').addClass('center-content')
+      // $('#workExperience').addClass('center-content')
+
+      bio.display();
+      work.display();
+      projects.display();
+      education.display();
